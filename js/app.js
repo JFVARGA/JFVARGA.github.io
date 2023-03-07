12d49b9b12c3a5d4964097326b6451d0a38f4231
JFVARGA <126497823+JFVARGA@users.noreply.github.com>
2023-03-06 22:48:32 -0300

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('boton_info').addEventListener('click', function() {
-    document.body.style.backgroundColor = 'black';
-});
-document.getElementById('boton_default').addEventListener('click', function() {
-    document.body.style.backgroundColor = 'white';
-});
